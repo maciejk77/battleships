0769a7ea2b4ef9ea3cece4da598c6f058cbb6c61
Maciej Kurek <maciejk77@gmail.com>
2015-03-23 12:02:39 +0000

    Set up Board class and Feature Test

---
+++ spec/board_spec.rb
@@ -0,0 +1,8 @@
+require 'board'
+
+describe Board do
+  it "can receive a ship" do
+    board = Board.new
+  end
+
+end
---
+++ spec/feature/battleships_spec.rb
@@ -0,0 +1,7 @@
+require 'capybara/rspec'
+feature 'Player can place a ship on board' do
+  scenario 'Player gets response when placing ship on board' do
+  board = Board.new
+  ship = board.place(ship)
+ end
+end
